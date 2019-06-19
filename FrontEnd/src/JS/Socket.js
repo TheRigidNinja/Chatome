ab0eb2d309733be0d6d3d9a2f9e59704070bc67c
brian shisanya <brian.shisanya2000@gmail.com>
2019-06-19 18:04:50 +1000

    added a script start

--- FrontEnd/src/JS/Socket.js
+++ FrontEnd/src/JS/Socket.js
@@ -2,7 +2,7 @@ import socketIOClient from "socket.io-client";
 
 // ----- // Socket connector handler
 const socket = (() => {
-  return socketIOClient("http://0315d2d6.ngrok.io/");//http://localhost:8080
+  return socketIOClient("https://git.heroku.com/chatome56.git");//http://localhost:8080
 })();
 
 export default socket;
