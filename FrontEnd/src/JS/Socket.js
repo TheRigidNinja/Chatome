1b5b50b4344c15ccc0b0cac5dabc33abaccd8ca0
Brian <brian.shisanya2000@gmail.com>
2019-07-28 12:57:51 +1000

    Updated Socket URL

--- FrontEnd/src/JS/Socket.js
+++ FrontEnd/src/JS/Socket.js
@@ -3,7 +3,7 @@ import socketIOClient from "socket.io-client";
 // ----- // Socket connector handler
 const socket = (() => {
     console.log("Connect");
-  return socketIOClient("http://7139dde6.ngrok.io/");//https://chatome56.herokuapp.com/
+  return socketIOClient("http://d7c5d76e.ngrok.io");//https://chatome56.herokuapp.com/
 })();
 
 export default socket;
